5024c572380ef86c67dc5169e62f7d79c4461822
jhutchines <44975942+jhutchines@users.noreply.github.com>
2018-11-12 16:44:45 +0000

    UI Prototype
    Attach the script to any button on the canvas and add GameObject

---
+++ Studio Prototypes/Assets/Scripts/JH_UI_Button.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JH_UI_Button : MonoBehaviour {
+
+    public GameObject uiPanel;
+    public bool openPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (openPanel) uiPanel.SetActive(true);  
+        else uiPanel.SetActive(false);  
+	}
+
+    public void ToggleUI()
+    {
+        if (openPanel) openPanel = false;
+        else
+        {
+            
+            for (int i = 0; i < uiPanel.transform.parent.childCount; i++)
+            {
+                if (transform.parent.GetChild(i).GetComponent<JH_UI_Button>() != null)
+                {
+                    if (transform.parent.GetChild(i) != gameObject)
+                    {
+                        transform.parent.GetChild(i).gameObject.GetComponent<JH_UI_Button>().openPanel = false;
+                    }
+                }
+            }
+            openPanel = true;
+        }
+    }
+}
